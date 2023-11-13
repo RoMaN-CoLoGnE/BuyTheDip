ef102a11ab1ac2c01514943976807fc710bc4eef
RoMaN-CoLoGnE <roman.fausel@icloud.com>
2023-11-13 15:26:32 +0100

    some changes

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,36 +1,33 @@
-// Set the target date and time
-const targetDate = new Date('2023-11-12T20:30:00Z').getTime();
+// Function to update elapsed time since the target date
+function updateElapsedTime() {
+  const targetDate = new Date('2023-11-12T20:30:00+01:00'); // Target date and time (12th November 2023, 8:30 PM MET)
+  const currentDate = new Date(); // Current date and time
 
-// Update the countdown function
-function updateCountdown() {
-  const currentDate = new Date().getTime();
-  const remainingTime = targetDate - currentDate;
+  const timeDifference = currentDate - targetDate; // Difference between the two times
 
-  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  if (timeDifference < 0) {
+    // The target date is in the future
+    document.querySelector('.countdown__time').textContent =
+      'The target date is in the future.';
+  } else {
+    // Calculate elapsed days, hours, minutes, and seconds
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-  const countdownElement = document.getElementById('countdown');
-  countdownElement.innerHTML =
-    days +
-    ' Days ' +
-    hours +
-    ' Hours ' +
-    minutes +
-    ' Minutes ' +
-    seconds +
-    ' Seconds ';
-
-  if (remainingTime <= 0) {
-    countdownElement.innerHTML = 'Countdown expired!';
+    // Update the countdown element
+    const countdownElement = document.querySelector('.countdown__time');
+    countdownElement.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
   }
 }
 
-// Initialize the countdown
-updateCountdown();
+// Update elapsed time every 1 second
+setInterval(updateElapsedTime, 1000);
 
-// Update the countdown every 1 second
-setInterval(updateCountdown, 1000);
+// Call the function when the page loads
+updateElapsedTime();
